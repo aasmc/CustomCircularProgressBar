9ee1593166dec3549cae9b91f7e062a3378990a6
Alex Alexeyev <aasmc@yandex.ru>
2022-08-10 21:49:39 +0300

    Complete CircularProgressBar

--- app/src/main/java/ru/aasmc/customcircularprogressbar/MainActivity.kt
+++ app/src/main/java/ru/aasmc/customcircularprogressbar/MainActivity.kt
@@ -3,12 +3,17 @@ package ru.aasmc.customcircularprogressbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import ru.aasmc.customcircularprogressbar.ui.theme.CustomCircularProgressBarTheme
 
@@ -17,27 +22,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CustomCircularProgressBarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    var value by remember {
+                        mutableStateOf(0)
+                    }
+                    CircularProgressBar(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if (it.isNotEmpty()) {
+                                it.toInt()
+                            } else {
+                                0
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CustomCircularProgressBarTheme {
-        Greeting("Android")
     }
 }
